66d4b582c19c1561767bc4597faed095c9f34d64
Haroon Abid <haroon.abid.1999@gmail.com>
2022-08-17 01:47:21 +0500

    rectangle callback and error handling

--- rectangle/index.js
+++ rectangle/index.js
@@ -2,12 +2,15 @@ let rect = require('./rectangle');
 
 const solveRect = (l, b) => {
     console.log(`Solving for rectangle with length = ${l} and breadth = ${b}`);
-    if (l <= 0 || b <= 0) {
-        console.log('Rectangle dimensions should be greater than zero: l = ' + l + ', b = ' + b);
-    } else {
-        console.log(`Perimeter = ${rect.perimiter(l, b)}`);
-        console.log(`Area = ${rect.area(l, b)}`);
-    }
+    rect(l, b, (err, rectangle) => {
+        if (err) {
+            console.log("Error:", err.message)
+        } else {
+            console.log(`The perimeter is ${rectangle.perimeter()} and the area is ${rectangle.area()} of rectangle with length = ${l} and breadth = ${b}`);
+        }
+
+    })
+    console.log("This is called after the call to the callback function");
 }
 
 solveRect(2, 4);
