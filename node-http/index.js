62964cb4b5d7cd1f5860bbcbefca970c591bcdeb
Haroon Abid <haroon.abid.1999@gmail.com>
2022-08-21 19:00:43 +0500

    node http server practice complete

--- node-http/index.js
+++ node-http/index.js
@@ -2,15 +2,46 @@ const HTTP = require('http')
 const HOSTNAME = 'localhost'
 const PORT = 3000
 
-const SERVER = HTTP.createServer((req, res) => {
-    // Log the request headers to the console
-    console.log(`Headers: ${JSON.stringify(req.headers)}`)
+const fs = require('fs');
+const path = require('path');
 
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/html')
-    res.end('<h1>Hello World</h1>')
+
+const server = HTTP.createServer((req, res) => {
+    console.log('Request for ' + req.url + ' by method ' + req.method);
+
+    if (req.method == 'GET') {
+        var fileUrl;
+        if (req.url == '/') fileUrl = '/index.html';
+        else fileUrl = req.url;
+
+        var filePath = path.resolve('./public' + fileUrl);
+        const fileExt = path.extname(filePath);
+        if (fileExt == '.html') {
+            fs.exists(filePath, (exists) => {
+                if (!exists) {
+                    res.statusCode = 404;
+                    res.setHeader('Content-Type', 'text/html');
+                    res.end('<html><body><h1>Error 404: ' + fileUrl +
+                        ' not found</h1></body></html>');
+                    return;
+                }
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/html');
+                fs.createReadStream(filePath).pipe(res);
+            });
+        } else {
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'text/html');
+            res.end('<html><body><h1>Error 404: ' + fileUrl +
+                ' not a HTML file</h1></body></html>');
+        }
+    } else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/html');
+        res.end('<html><body><h1>Error 404: ' + req.method +
+            ' not supported</h1></body></html>');
+    }
 })
-
-SERVER.listen(PORT, HOSTNAME, () => {
+server.listen(PORT, HOSTNAME, () => {
     console.log(`Server running at http://${HOSTNAME}:${PORT}/`)
 })
